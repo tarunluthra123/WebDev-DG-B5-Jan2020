77d6c822263f08d458b3c5483991ad77415edc53
Tarun Luthra <tarunluthra987@gmail.com>
2021-01-20 14:55:42 +0530

    To do list

--- Project - To do list/script.js
+++ Project - To do list/script.js
@@ -14,22 +14,35 @@ addTaskBtn.onclick = function () {
 	const listItem = document.createElement("li");
 	const crossBtn = document.createElement("button");
 	const upBtn = document.createElement("button");
+	const downBtn = document.createElement("button");
 
 	crossBtn.innerText = "X";
 	upBtn.innerText = "↑";
+	downBtn.innerText = "↓";
+
+	// Assign classes for CSS
+	listItem.classList.add("task");
+	crossBtn.classList.add("crossBtn");
+	upBtn.classList.add("upBtn");
+	downBtn.classList.add("downBtn");
 
 	// We want that when the user clicks on this crossBtn, the listItem should be removed
-
 	crossBtn.onclick = function () {
 		listItem.remove();
 	};
 
+	// listItem should be moved up when user clicks on up button
 	upBtn.onclick = function () {
 		taskList.insertBefore(listItem, listItem.previousElementSibling);
 	};
 
+	downBtn.onclick = function () {
+		taskList.insertBefore(listItem.nextElementSibling, listItem);
+	};
+
 	listItem.append(crossBtn);
 	listItem.append(upBtn);
+	listItem.append(downBtn);
 
 	listItem.append(newTask);
 
